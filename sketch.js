9704a36d52ffb0526d8a0182806da54d48f2da0b
monicabocelli <monica.bocelli93@gmail.com>
2017-02-25 10:26:29 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,9 +13,9 @@ var fontMetaRegular, fontMetaBold, fontMetaMedium, fontOCRB;
 function preload() {
    Cover = loadImage("images/gif-a-2-orizzontale.gif");
    
-   Chile = loadImage("images/prova1.jpg");    //9.5
-   Japan = loadImage("images/prova2.jpg");    //9.0
-   Indonesia = loadImage("images/prova2.jpg"); //8.5
+   Chile = loadImage("images/Chile2.jpg");    //9.5
+   Japan = loadImage("images/Japan.jpg");    //9.0
+   Indonesia = loadImage("images/Indonesia.jpg"); //8.5
    
    fontMetaRegular = loadFont("assets/MetaPro-Normal.otf");
    fontMetaBold = loadFont("assets/MetaPro-Bold.otf");
